62c75ac2b473614efb56de56845c5dc2efcb17eb
GloriaBargoria <bargoriagloria@gmail.com>
2024-05-02 22:42:53 +0300

    added redirect url

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,12 @@ const manifestUrl =
   "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <TonConnectUIProvider manifestUrl={manifestUrl}>
+  <TonConnectUIProvider
+    manifestUrl={manifestUrl}
+    actionsConfiguration={{
+      twaReturnUrl: "https://t.me/mobifi_bot/mobifi",
+    }}
+  >
     <App />
   </TonConnectUIProvider>
 );
