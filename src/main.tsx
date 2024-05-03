cf3628d7ccbefda37f98714aaddb625ae62d9c71
GloriaBargoria <bargoriagloria@gmail.com>
2024-05-03 10:28:42 +0300

    fix manifest url not found

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,9 @@ import "./index.css";
 import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 
-const manifestUrl =
-  "https://t.me/mobifi_bot/mobifi/tonconnect-manifest.json";
+// link to manifest file in project, make sure the url is exact
+// pass an exact 180 * 180px icon image
+const manifestUrl = "https://t.me/mobifi_bot/mobifi/tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <TonConnectUIProvider
@@ -13,6 +14,24 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     actionsConfiguration={{
       // return strategy as a link to your TMA that will be only applied if the App is opened in TMA mode
       twaReturnUrl: "https://t.me/mobifi_bot/mobifi",
+    }}
+
+    // list of wallets in the connect wallet modal, in our case, we only need TON wallet
+    // exlude this parameter to list all wallets
+    walletsListConfiguration={{
+      includeWallets: [
+        {
+          appName: "tonwallet",
+          name: "TON Wallet",
+          imageUrl: "https://wallet.ton.org/assets/ui/qr-logo.png",
+          aboutUrl:
+            "https://chrome.google.com/webstore/detail/ton-wallet/nphplpgoakhhjchkkhmiggakijnkhfnd",
+          universalLink: "https://wallet.ton.org/ton-connect",
+          jsBridgeKey: "tonwallet",
+          bridgeUrl: "https://bridge.tonapi.io/bridge",
+          platforms: ["chrome", "android"],
+        },
+      ],
     }}
   >
     <App />
